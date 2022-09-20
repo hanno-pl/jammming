b0ada7bbcb8390448b0a959fa246deda349d8671
Hanno Pleis <hanno@ulfertsprygoda.de>
2022-09-20 15:40:01 +0200

    change playlist name

--- src/Components/App/App.js
+++ src/Components/App/App.js
@@ -1,7 +1,7 @@
 import './App.css';
-import {SearchBar} from '../SearchBar/SearchBar';
-import {SearchResults} from '../SearchResults/SearchResults'
-import {Playlist} from '../Playlist/Playlist';
+import { SearchBar } from '../SearchBar/SearchBar';
+import { SearchResults } from '../SearchResults/SearchResults'
+import { Playlist } from '../Playlist/Playlist';
 import React from "react"
 
 class App extends React.Component {
@@ -9,57 +9,65 @@ class App extends React.Component {
     super(props)
     this.state = {
       searchResults: [{
-      name: "first song",
-      artist: "first artist",
-      album: "first album",
-      id: "1"
-    },
-    {
-      name: "second song",
-      artist: "second artist",
-      album: "second album",
-      id: "2"
-    },{
-      name: "third song",
-      artist: "third artist",
-      album: "third album",
-      id: "3"
-    }], 
-  playlistName: "testPlaylist", 
-  playlistTracks: [
-    {
-      name: "first playlist song",
-      artist: "first playlist artist",
-      album: "first playlistalbum",
-      id: "p1"
-    },
-    {
-      name: "second playlist song",
-      artist: "second playlist artist",
-      album: "second playlist album",
-      id: "p2"
-    },{
-      name: "third playlist song",
-      artist: "third playlist artist",
-      album: "third playlist album",
-      id: "p3"
+        name: "first song",
+        artist: "first artist",
+        album: "first album",
+        id: "1"
+      },
+      {
+        name: "second song",
+        artist: "second artist",
+        album: "second album",
+        id: "2"
+      }, {
+        name: "third song",
+        artist: "third artist",
+        album: "third album",
+        id: "3"
+      }],
+      playlistName: "testPlaylist",
+      playlistTracks: [
+        {
+          name: "first playlist song",
+          artist: "first playlist artist",
+          album: "first playlistalbum",
+          id: "p1"
+        },
+        {
+          name: "second playlist song",
+          artist: "second playlist artist",
+          album: "second playlist album",
+          id: "p2"
+        }, {
+          name: "third playlist song",
+          artist: "third playlist artist",
+          album: "third playlist album",
+          id: "p3"
+        }
+      ]
     }
-  ]}
     this.addTrack = this.addTrack.bind(this)
-    this.removeTrack = this.removeTrack.bind(this)
+    this.removeTrack = this.removeTrack.bind(this)    
+    this.updatePlaylistName = this.updatePlaylistName.bind(this)
   }
 
-  addTrack(track){
+  addTrack(track) {
     let tracks = this.state.playlistTracks
     if (this.state.playlistTracks.find(savedTrack => savedTrack.id === track.id)) {
       return;
     }
     tracks.push(track)
-    this.setState({playlistTracks: tracks})}
+    this.setState({ playlistTracks: tracks })
+  }
 
-    removeTrack(track){
-      let tracks = this.state.playlistTracks.filter(playlistTrack => playlistTrack.id !== track.id)   
-      this.setState({playlistTracks: tracks})}
+  removeTrack(track) {
+    let tracks = this.state.playlistTracks.filter(playlistTrack => playlistTrack.id !== track.id)
+    this.setState({ playlistTracks: tracks })
+  }
+
+  updatePlaylistName(name) {
+    this.setState({playlistName: name})
+  }
 
 
   render() {
@@ -69,8 +77,12 @@ class App extends React.Component {
         <div className="App">
           <SearchBar />
           <div className="App-playlist">
-            <SearchResults searchResults={this.state.searchResults} onAdd={this.addTrack}/>
-            <Playlist playlistName={this.state.playlistName} playlistTracks={this.state.playlistTracks} onRemove={this.removeTrack}/>
+            <SearchResults searchResults={this.state.searchResults} onAdd={this.addTrack} />
+            <Playlist 
+            playlistName={this.state.playlistName}
+            playlistTracks={this.state.playlistTracks} 
+            onRemove={this.removeTrack} 
+            onNameChange={this.updatePlaylistName}/>
           </div>
         </div>
       </div>
