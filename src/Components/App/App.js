a5b050769e3299f2b07d54fe409ce8dbfd4a1661
Hanno Pleis <hanno@ulfertsprygoda.de>
2022-09-20 14:19:38 +0200

    render search result

--- src/Components/App/App.js
+++ src/Components/App/App.js
@@ -1,8 +1,32 @@
 import './App.css';
 import {SearchBar} from '../SearchBar/SearchBar';
+import {SearchResults} from '../SearchResults/SearchResults'
+import {Playlist} from '../Playlist/Playlist';
 import React from "react"
 
 class App extends React.Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      searchResults: [{
+      name: "first song",
+      artist: "first artist",
+      album: "first album",
+      id: "1"
+    },
+    {
+      name: "second song",
+      artist: "second artist",
+      album: "second album",
+      id: "2"
+    },{
+      name: "third song",
+      artist: "third artist",
+      album: "third album",
+      id: "3"
+    }]}
+  }
+
   render() {
     return (
       <div>
@@ -10,8 +34,8 @@ class App extends React.Component {
         <div className="App">
           <SearchBar />
           <div className="App-playlist">
-            {/*<SearchResults />*/}
-            {/*<Playlist />*/}
+            <SearchResults searchResults={this.state.searchResults}/>
+            <Playlist />
           </div>
         </div>
       </div>
