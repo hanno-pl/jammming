9f4ec277924657ff4f94a85c7d204e0afbd5ddf3
Hanno Pleis <hanno@ulfertsprygoda.de>
2022-09-20 15:20:47 +0200

    remove tracks

--- src/Components/App/App.js
+++ src/Components/App/App.js
@@ -46,6 +46,7 @@ class App extends React.Component {
     }
   ]}
     this.addTrack = this.addTrack.bind(this)
+    this.removeTrack = this.removeTrack.bind(this)
   }
 
   addTrack(track){
@@ -56,6 +57,11 @@ class App extends React.Component {
     tracks.push(track)
     this.setState({playlistTracks: tracks})}
 
+    removeTrack(track){
+      let tracks = this.state.playlistTracks.filter(playlistTrack => playlistTrack.id !== track.id)   
+      this.setState({playlistTracks: tracks})}
+
+
   render() {
     return (
       <div>
@@ -64,7 +70,7 @@ class App extends React.Component {
           <SearchBar />
           <div className="App-playlist">
             <SearchResults searchResults={this.state.searchResults} onAdd={this.addTrack}/>
-            <Playlist playlistName={this.state.playlistName} playlistTracks={this.state.playlistTracks}/>
+            <Playlist playlistName={this.state.playlistName} playlistTracks={this.state.playlistTracks} onRemove={this.removeTrack}/>
           </div>
         </div>
       </div>
